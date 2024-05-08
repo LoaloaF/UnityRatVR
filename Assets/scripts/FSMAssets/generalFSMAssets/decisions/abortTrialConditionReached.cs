ee7bd14a4c149b726ec0212f4ff9511b8e9be679
Your Name <you@example.com>
2024-05-08 17:20:30 +0200

    abort rest to 0s fixed

--- Assets/scripts/FSMAssets/generalFSMAssets/decisions/abortTrialConditionReached.cs
+++ Assets/scripts/FSMAssets/generalFSMAssets/decisions/abortTrialConditionReached.cs
@@ -15,29 +15,14 @@ namespace Experiment.ExperimentFSM
         private float currentTime;
         private bool firstDecicionCall = true;
         public override bool Decide(BaseStateMachine stateMachine)
-        {
-            // Debug.Log("Time.realtimeSinceStartup:"+Time.realtimeSinceStartup+ " t0:"+t0+" firstDecicionCall: "+firstDecicionCall);
-            if (firstDecicionCall)
-            {
-                // Debug.Log("FIRSTabortTrialConditionReached");
-                t0 = Time.realtimeSinceStartup;
-                currentTime = t0;
-                firstDecicionCall = false;
-            }
-            
-            Debug.Log(Time.realtimeSinceStartup-t0);
-            Debug.Log(stateMachine._sessionManager.maximumTrialLength);
-            
-            if (Time.realtimeSinceStartup-t0 > 
+        {            
+            if (Time.realtimeSinceStartup-stateMachine._sessionManager.trialStartTimestamp > 
                 stateMachine._sessionManager.maximumTrialLength) {
-                firstDecicionCall = true;
                 return true;
             }
             return false;
         }
-
     }
-
 }
 
 
